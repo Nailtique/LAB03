43117aed26e22c34ef71b3ef5833a662b68164e0
SaraAwad1 <>
2021-05-06 07:32:11 +0000

    add app

--- app.js
+++ app.js
@@ -3,3 +3,16 @@
  comments 
  // this code is for showing a messgae for the user
  prompt = my house 
+ var x = 5
+ var firstName = 'sara'
+ alert(firstName)
+
+ var age = promt ('what is your age?')
+ Mohammed = 'welcome to my site'
+
+ if(age > 18) {
+   mohammad = 'sorry you are not allowed' 
+
+ }else{ 
+   alert('have a good day')
+ }
