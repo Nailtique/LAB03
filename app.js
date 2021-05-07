3991aa151bcd1a80f8fdf5af7423cf6172c62f56
SaraAwad1 <>
2021-05-07 10:20:30 +0000

    app

---
+++ app.js
@@ -0,0 +1,29 @@
+
+alert ('helo sara is here ..');
+//  comments
+ // this code is for showing a messgae for the user
+//  prompt = my house
+var sara =prompt('plase enter you number');
+var amro =confirm('are you dog');
+ var x = 5
+ var firstName = 'sara'
+ alert(firstName)
+ var age = promt ('what is your age?')
+ //Mohammed = 'welcome to my site'
+ if(age > 18) {
+   mohammad = 'sorry you are not allowed'
+ }else{
+   alert('have a good day')
+ }
+ if (age>= 18 && age < 35){msg= 'welcome to my site'}
+ else if (age <18 || age > 35 ){msg= 'sorry you are not allowed'}else{
+   alert('have a nice day')
+ }
+white_check_mark
+eyes
+raised_hands
+
+
+
+
+
